4bb96e2e5cc5ae867ece773915c969d68d32e20b
USER <USER@INET-20>
2018-03-13 18:10:55 +0900

    ffffff

---
+++ EX099/src/Ex01.java
@@ -0,0 +1,18 @@
+
+public class Ex01 {
+
+	String mon;
+	int year;
+	int month;
+	int day;
+	
+	
+	void print1(){
+		System.out.println(year+"."+month+"."+day);
+	}
+	void print2(){
+			System.out.println(year+"."+mon+"."+day);
+		}
+	}
+
+
---
+++ EX099/src/Ex01Test.java
@@ -0,0 +1,16 @@
+
+public class Ex01Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ex01 d;
+		d = new Ex01();
+		d.year = 2012;
+		d.month = 7;
+		d.day = 5;
+		d.mon = "jun";
+		d.print1();
+		d.print2();
+	}
+
+}
